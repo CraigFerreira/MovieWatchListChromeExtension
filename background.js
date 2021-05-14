87c403e4694776640a877b9256329bf065c5c099
Craig Ferreira <ferreira.craig@gmail.com>
2021-05-14 18:38:44 -0400

    new shows rendering to screen with only one click

--- background.js
+++ background.js
@@ -7,6 +7,7 @@ chrome.runtime.onInstalled.addListener((details) => {
     fetch(` http://api.tvmaze.com/schedule/web?date=2021-05-13&country=US`)
     .then(res => res.json())
     .then(data => {
+        console.log('show data',data)
         chrome.storage.local.set({
             shows: data,
         })  
--- popup/popup.js
+++ popup/popup.js
@@ -23,29 +23,37 @@ chrome.storage.local.get(["shows"], (res) => {
 
 window.onclick = function(event) {
     var target = event.target ;
-    if(target.tagName==='BUTTON' && target.id==='date-submit'){
-        console.log('date value', date.value)
+    
+    if(target.tagName==='BUTTON' && target.id==='date-submit' && date.value!==''){
+        console.log('date value', typeof date.value)
         fetch(` http://api.tvmaze.com/schedule/web?date=${date.value}&country=US`)
         .then(res => res.json())
         .then(data => {
-            chrome.storage.local.set({
-                shows: data,
-            })  
-        })
-
-        movieList.innerHTML=''
-        chrome.storage.local.get(["shows"], (res) => {
-            // console.log('shows',res.shows)
-            for (let i=0; i<res.shows.length; i++) {
-                let show=res.shows[i]
+            // chrome.storage.local.set({
+            //     shows: data,
+            // })  
+            movieList.innerHTML=''
+            for (let i=0; i<data.length; i++) {
+                let show=data[i]
                 // console.log('url', show.url)
                 if(!show.summary || !show.image) continue
-                movieList.innerHTML+=`<li class='list-item'>Name: ${show.name} <br/> Description: ${show.summary} <br/> <button name='${show.name}' id='link${i}'>Watch Preview</button> </li>`
-        
+                movieList.innerHTML+=`<li class='list-item'>Name: ${show.name} <br/> Description: ${show.summary} <br/> <button name='${show.name}' id='link${i}' season='${show.season}' episode='${show.episode}'>Watch Preview</button> </li>`
             }
-        
-        
         })
+
+        // movieList.innerHTML=''
+        // chrome.storage.local.get(["shows"], (res) => {
+            // console.log('shows',res.shows)
+            // for (let i=0; i<res.shows.length; i++) {
+            //     let show=res.shows[i]
+            //     // console.log('url', show.url)
+            //     if(!show.summary || !show.image) continue
+            //     movieList.innerHTML+=`<li class='list-item'>Name: ${show.name} <br/> Description: ${show.summary} <br/> <button name='${show.name}' id='link${i}' season=${show.season} episode=${show.episode}>Watch Preview</button> </li>`
+        
+            // }
+        
+        
+        // })
 
 
 
@@ -55,7 +63,7 @@ window.onclick = function(event) {
     
     else if(target.tagName==='BUTTON'&& target.id!=='date-submit'){
         chrome.tabs.create({
-            url: `https://www.youtube.com/results?search_query=${target.name}`
+            url: `https://www.youtube.com/results?search_query=${target.name}+season+${target.season}`
         })
     }
 }
